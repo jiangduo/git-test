3f91bdfc00b317f9cd2b2912946d91a68e3e2f24
Totoro <Totoro@Jiang.com>
2022-12-04 15:49:30 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author Totoro
+ * @create 04 15:38
+ * @Description: 这是一个测试文件
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("first -test ");
+    }
+}
